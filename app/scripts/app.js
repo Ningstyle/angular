7c3c43eeabbc78d07c80889c065ff04ea4a8f507
lining <1046674804@qq.com>
2016-11-11 20:18:54 +0800

    版本一

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name weekworkApp
+ * @description
+ * # weekworkApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('weekworkApp', ["ui.router"]).config(function($stateProvider,$urlRouterProvider){
+	$urlRouterProvider.when("","/fon1")
+	$stateProvider.state("fon1",{
+		url:"/fon1",
+		templateUrl:"views/fon1.html",
+		controller:"fon1",
+	}).state("fon2",{
+		url:"/fon2",
+		templateUrl:"views/fon2.html",
+		controller:"fon2"
+	})
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weekworkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weekworkApp
+ */
+angular.module('weekworkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
